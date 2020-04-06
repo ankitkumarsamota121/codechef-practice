97b8e318f3e60726a5300565943d08e65bbe6419
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-04-06 23:28:51 +0530

    Solving LEARNDSA01

--- LEARNDSA/LEARNDSA01/ZCO14003.cpp
+++ LEARNDSA/LEARNDSA01/ZCO14003.cpp
@@ -3,11 +3,20 @@ using namespace std;
 
 int main()
 {
-    int t{};
-    cin >> t;
-    while (t--)
+    long long n{};
+    cin >> n;
+    vector<long long> customers(n);
+    for (auto &customer : customers)
+        cin >> customer;
+
+    sort(customers.begin(), customers.end());
+
+    long long max{0};
+    for (int i{0}, j{n}; i < n; ++i, --j)
     {
-        
+        if (customers[i] * j >= max)
+            max = customers[i] * j;
     }
+    cout << max << '\n';
     return 0;
 }
