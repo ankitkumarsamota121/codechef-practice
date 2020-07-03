fadc310bed695b79df73e59f9f644b27ced8838f
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-07-03 23:17:16 +0530

    Solving JULY20B

---
+++ LongChallenge/JULY20B/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<int, int> m;
+    m[1] = 1;
+    m[3] = 1;
+    m[4] = 1;
+    m[5] = 2;
+
+    cout << m[2] << endl;
+
+    return 0;
+}
